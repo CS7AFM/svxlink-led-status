cfa3e38bc9d30eb0303189fbba6a38177ccd1593
Dimitri <35607641+f5swb@users.noreply.github.com>
2020-08-09 08:53:24 +0200

    Add files via upload

---
+++ svxstatut.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# script de gestion led orange status svxlink 
+# F5SWB 2020 
+
+LED3=23
+
+cd /sys/class/gpio/
+
+if [ -e /sys/class/gpio/export ]
+then
+        echo "$LED3" > /sys/class/gpio/export
+fi
+
+echo out > /sys/class/gpio/gpio$LED3/direction
+echo 0 > /sys/class/gpio/gpio$LED3/value
+
+
+# ici on lance une boucle 
+# on récupère le process de svxlink 
+# si il est égal à zero on éteind la led sinon on l'a fait clignoter
+
+while true;
+do
+
+svxlink=$(ps -ef | grep svxlink | grep -v "grep" | wc -l)
+if [ $svxlink -eq 0 ]; # If process svxlink is not running
+   then
+    echo 0 > /sys/class/gpio/gpio$LED3/value
+    else
+    # timer illumination 	
+    echo 1  > /sys/class/gpio/gpio$LED3/value
+    sleep 2
+    #timer extinction
+    echo 0 > /sys/class/gpio/gpio$LED3/value
+    sleep 0.4
+fi
+
+
+done
+
+exit 0
